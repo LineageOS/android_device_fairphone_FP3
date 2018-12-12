38f538a8d8a45aa1e4ee8f2a485b3d2768cdc55c
BeYkeRYkt <beykerykt@gmail.com>
2018-12-13 06:42:35 +0900

    sdm632-common: power: Handle launch and interaction hints
     * MP-CTL does not handle POWER_HINT_INTERACTION and POWER_HINT_LAUNCH
       directly. Requests for processing are sent by Qualcomm BoostFramework
       (QPerformance) at the framework service level. Since we do not have
       BoostFramework, process POWER_HINT_INTERACTION and POWER_HINT_LAUNCH
       in PowerHAL to ensure a sufficient level of performance.
     * For proper operation, perfboostsconfig.xml file is required.
    
    Change-Id: Ic632fb71cdae7ae3b50a122685e222c1fa21203f

--- power/power-8953.c
+++ power/power-8953.c
@@ -31,6 +31,7 @@
 #define LOG_NIDEBUG 0
 
 #include <errno.h>
+#include <time.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -51,6 +52,10 @@
 
 #define CHECK_HANDLE(x) ((x)>0)
 #define NUM_PERF_MODES  3
+
+const int kMaxInteractiveDuration = 5000; /* ms */
+const int kMinInteractiveDuration = 500; /* ms */
+const int kMinFlingDuration = 1500; /* ms */
 
 static int current_power_profile = PROFILE_BALANCED;
 
@@ -270,6 +275,51 @@ static int process_video_encode_hint(void *metadata)
     return HINT_NONE;
 }
 
+static int process_activity_launch_hint(void *UNUSED(data))
+{
+    if (current_mode != NORMAL_MODE) {
+        ALOGV("%s: ignoring due to other active perf hints", __func__);
+    } else {
+        perf_hint_enable_with_type(VENDOR_HINT_FIRST_LAUNCH_BOOST, -1, LAUNCH_BOOST_V1);
+    }
+    return HINT_HANDLED;
+}
+
+static int process_interaction_hint(void *data)
+{
+    static struct timespec s_previous_boost_timespec;
+    static int s_previous_duration = 0;
+    struct timespec cur_boost_timespec;
+    long long elapsed_time;
+    int duration = kMinInteractiveDuration;
+    if (current_mode != NORMAL_MODE) {
+        ALOGV("%s: ignoring due to other active perf hints", __func__);
+        return HINT_HANDLED;
+    }
+    if (data) {
+        int input_duration = *((int*)data);
+        if (input_duration > duration) {
+            duration = (input_duration > kMaxInteractiveDuration) ?
+                    kMaxInteractiveDuration : input_duration;
+        }
+    }
+    clock_gettime(CLOCK_MONOTONIC, &cur_boost_timespec);
+    elapsed_time = calc_timespan_us(s_previous_boost_timespec, cur_boost_timespec);
+    // don't hint if previous hint's duration covers this hint's duration
+    if ((s_previous_duration * 1000) > (elapsed_time + duration * 1000)) {
+        return HINT_HANDLED;
+    }
+    s_previous_boost_timespec = cur_boost_timespec;
+    s_previous_duration = duration;
+    if (duration >= kMinFlingDuration) {
+        // Use launch boost resources for fling boost
+        perf_hint_enable_with_type(VENDOR_HINT_FIRST_LAUNCH_BOOST, -1, LAUNCH_BOOST_V1);
+    } else {
+        perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
+    }
+    return HINT_HANDLED;
+}
+
 int power_hint_override(power_hint_t hint, void *data)
 {
     int ret_val = HINT_NONE;
@@ -297,6 +347,12 @@ int power_hint_override(power_hint_t hint, void *data)
             break;
         case POWER_HINT_VR_MODE:
             ret_val = process_perf_hint(data, VR_MODE);
+            break;
+        case POWER_HINT_INTERACTION:
+            ret_val = process_interaction_hint(data);
+            break;
+        case POWER_HINT_LAUNCH:
+            ret_val = process_activity_launch_hint(data);
             break;
         default:
             break;
