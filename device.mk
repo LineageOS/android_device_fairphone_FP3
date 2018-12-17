834f5cc6abfbcc32f8a74c6917aa3d467985408a
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    FP3: Adding Audio HAL V5: Direction API
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- device.mk
+++ device.mk
@@ -216,9 +216,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0 \
     android.hardware.audio.common@4.0 \
     android.hardware.audio.common@4.0-util \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl
 
 # Audio
 PRODUCT_PACKAGES += \
