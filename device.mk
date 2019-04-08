793584eb732004256b03bc47b3ed08923820b27d
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    FP3: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/fairphone/FP3/FP3-vendor.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
