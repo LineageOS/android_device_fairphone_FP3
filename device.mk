99792d3e038c1d1b3e8b4d2d3b563cd263956154
Wang Han <wanghan1995315@gmail.com>
2019-08-12 11:54:09 +0800

    FP3: Remove libaacwrapper from PRODUCT_PACKAGES
     * It does not exist at all and is replaced by libwfdaac.
    
    Change-Id: I3b2684b9980d6856f1817e5a40e42861ea86ae24

--- device.mk
+++ device.mk
@@ -86,7 +86,6 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
-    libaacwrapper \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
