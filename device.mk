57f81a736f6214f530d0ab59d460904d5a7df858
Pranav Vashi <neobuddy89@gmail.com>
2019-10-06 11:32:22 +0530

    FP3: Switch to 1.2 version of qcom power HAL
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I386f42425e612eef7a14310bb4ca07a54efad272

--- device.mk
+++ device.mk
@@ -378,7 +378,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Thermal
 PRODUCT_PACKAGES += \
