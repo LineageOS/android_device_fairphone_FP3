1bf398d87e12dc4a084dfdd1c5601d22c63453a4
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    FP3: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- device.mk
+++ device.mk
@@ -493,13 +493,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libprotobuf-cpp-full
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
