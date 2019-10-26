a9e54b27de49adef358145dfa47c4fe2c1a71426
Erfan Abdi <erfangplus@gmail.com>
2019-10-26 23:55:05 +0330

    sdm632-common: Update wlan firmware symlinks

--- Android.mk
+++ Android.mk
@@ -31,6 +31,56 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := wifi_symlinks
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): ACTUAL_INI_FILE := /data/vendor/wifi/WCNSS_qcom_cfg.ini
+$(LOCAL_BUILT_MODULE): WCNSS_INI_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+
+$(LOCAL_BUILT_MODULE): ACTUAL_BIN_FILE := /vendor/etc/wifi/WCNSS_qcom_wlan_nv.bin
+$(LOCAL_BUILT_MODULE): WCNSS_BIN_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+$(LOCAL_BUILT_MODULE): ACTUAL_BIN_ARG_FILE := /vendor/etc/wifi/WCNSS_qcom_wlan_nv_Argentina.bin
+$(LOCAL_BUILT_MODULE): WCNSS_BIN_ARG_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv_Argentina.bin
+
+$(LOCAL_BUILT_MODULE): ACTUAL_BIN_BRZ_FILE := /vendor/etc/wifi/WCNSS_qcom_wlan_nv_Brazil.bin
+$(LOCAL_BUILT_MODULE): WCNSS_BIN_BRZ_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv_Brazil.bin
+
+$(LOCAL_BUILT_MODULE): ACTUAL_BIN_IND_FILE := /vendor/etc/wifi/WCNSS_qcom_wlan_nv_India.bin
+$(LOCAL_BUILT_MODULE): WCNSS_BIN_IND_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv_India.bin
+
+$(LOCAL_BUILT_MODULE): ACTUAL_BIN_EPA_FILE := /vendor/etc/wifi/WCNSS_qcom_wlan_nv_epa.bin
+$(LOCAL_BUILT_MODULE): WCNSS_BIN_EPA_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv_epa.bin
+
+$(LOCAL_BUILT_MODULE): ACTUAL_DAT_FILE := /vendor/etc/wifi/WCNSS_wlan_dictionary.dat
+$(LOCAL_BUILT_MODULE): WCNSS_DAT_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
+
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Making symlinks for wifi"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(WCNSS_INI_SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(WCNSS_INI_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_INI_FILE) $(WCNSS_INI_SYMLINK)
+	$(hide) rm -rf $(WCNSS_BIN_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_BIN_FILE) $(WCNSS_BIN_SYMLINK)
+	$(hide) rm -rf $(WCNSS_BIN_ARG_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_BIN_ARG_FILE) $(WCNSS_BIN_ARG_SYMLINK)
+	$(hide) rm -rf $(WCNSS_BIN_BRZ_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_BIN_BRZ_FILE) $(WCNSS_BIN_BRZ_SYMLINK)
+	$(hide) rm -rf $(WCNSS_BIN_IND_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_BIN_IND_FILE) $(WCNSS_BIN_IND_SYMLINK)
+	$(hide) rm -rf $(WCNSS_BIN_EPA_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_BIN_EPA_FILE) $(WCNSS_BIN_EPA_SYMLINK)
+	$(hide) rm -rf $(WCNSS_DAT_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_DAT_FILE) $(WCNSS_DAT_SYMLINK)
+	$(hide) touch $@
+
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware; \
     ln -sf /dev/block/bootdevice/by-name/msadp \
         $(TARGET_OUT_VENDOR)/firmware/msadp)
