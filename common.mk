499dddd5c1e14ceaa126c617cdba179e11bd0c09
SyberHexen <SyberHexen@gmail.com>
2019-10-27 23:07:06 -0700

    sdm632-common: Fix StagefrightPluginLoader
    Fixes this
    StagefrightPluginLoader: Failed to load library: libstagefright_ccodec.so (dlopen failed: library "libstagefright_ccodec.so" not found)
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- common.mk
+++ common.mk
@@ -271,6 +271,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Media / StagefrightCodec 2.0
+PRODUCT_PACKAGES += \
+    libstagefright_ccodec
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
