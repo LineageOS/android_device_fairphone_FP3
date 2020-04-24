a05201b6f7bb3f4de1788a6f4b4117a0b3410f34
Sooraj S <sooorajjj@gmail.com>
2020-04-24 15:40:48 +0530

    FP3: Add Ambient Display support
    * Enable Automatic brightness by default
    * Source code for Doze taken from https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/lineage-16.0/parts

--- device.mk
+++ device.mk
@@ -361,6 +361,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.weaver@1.0
 
+# Doze
+PRODUCT_PACKAGES += \
+    Doze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
