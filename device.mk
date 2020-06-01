cfdda6794fbbc748c7a3b65ae40ecefd3bcae012
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2020-06-01 13:13:08 +0530

    FP3: Remove deprecated android.hardware.thermal@1.0 HAL
    Remove deprecated android.hardware.thermal@1.0 HAL from
    device manifest.
    
    Bug: 154012150
    Change-Id: I884e6b96cac4ebc3abcfaf309388d526a30dc5bd

--- device.mk
+++ device.mk
@@ -398,11 +398,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
