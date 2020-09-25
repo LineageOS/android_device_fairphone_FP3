500dfa4ed99129128f00b6ad9a08d37c3887f0c6
sooorajjj <sooorajjj@gmail.com>
2020-09-25 09:07:47 +0200

    FP3: Build DRM package

--- device.mk
+++ device.mk
@@ -290,6 +290,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.weaver@1.0
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
