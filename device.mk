ee5290d0ec059c5e568bd556487f347c738081c2
Sooraj S <sooorajjj@gmail.com>
2020-10-20 16:46:47 +0530

    FP3: Add telephony_product_privapp-permissions-qti.xml to product
    Change-Id: I792f1cfa804040a6d40880938776bcc77827ece5

--- device.mk
+++ device.mk
@@ -195,7 +195,8 @@ PRODUCT_COPY_FILES += \
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
