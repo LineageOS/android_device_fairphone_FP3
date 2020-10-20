695946ccca43fc3070db56c76ce5f61eef9a0bd1
Sooraj S <sooorajjj@gmail.com>
2020-10-20 12:03:39 +0200

    FP3: update init scripts from stock FP3 Q
    Change-Id: I69cc51f3e6b83ed22219a878bc3439c024a60108

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -322,3 +322,11 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/$(LOCAL_MODULE)
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.time_daemon.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.time_daemon.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
