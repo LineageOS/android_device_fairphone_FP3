65bcc4c1ebcd0fbe4b0518cd69050686aa2d69fc
Sooraj S <sooorajjj@gmail.com>
2020-11-29 10:42:12 +0100

    FP3: Introduce ODM manifest for fingerprint
    Change-Id: I3df169380560683d4aba3199178c4b5e325a074c

--- device.mk
+++ device.mk
@@ -332,6 +332,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1 \
     android.hardware.biometrics.fingerprint@2.1-service
 
 # FBE support
