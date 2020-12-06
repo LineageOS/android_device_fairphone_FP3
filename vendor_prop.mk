fb14f3f544da78545e44191be336dc5849d74fab
Sooraj S <sooorajjj@gmail.com>
2020-12-06 10:43:44 +0100

    FP3: Update graphics blobs and config
    * from FP3-REL-Q-3.A.0054-20200929.092252
    
    Change-Id: I37c8940675fecccc1e5e2046e1bf03cb155ff94a

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -9,6 +9,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     dev.pm.dyn_samplingrate=1 \
     debug.sf.recomputecrop=0 \
+    debug.sf.disable_backpressure=1 \
     persist.hwc.mdpcomp.enable=true \
     debug.mdpcomp.logs=0
 
@@ -285,6 +286,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.data.iwlan.enable=true \
     persist.dbg.wfc_avail_ovr=1
+
+# HWUI properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
 
 # Enable TDES support when KM4 is enabled
 PRODUCT_PROPERTY_OVERRIDES += \
