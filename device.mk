8a1a5a919d091dfc78ecbb46a6bdbd3e57afe15b
Sal Savage <salsavage@google.com>
2022-02-04 09:40:38 -0800

    FP3: Enable bluetooth profiles with product.prop
    Bug: 217448211
    Test: make -j
    Change-Id: I0d06b46910e42346653bea3484636f4129592ce3
    (cherry picked from commit 60b1e9984f023902b06ebc1479e84a1201083688)

--- device.mk
+++ device.mk
@@ -154,9 +154,29 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Bluetooth Properties
-# Hearing Aid Audio Support Using Bluetooth LE
+# Set supported Bluetooth profiles to enabled
 PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.profile.asha.central.enabled=true
+    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.bap.broadcast.assist.enabled=true \
+    bluetooth.profile.bap.unicast.server.enabled=true \
+    bluetooth.profile.bas.client.enabled=true \
+    bluetooth.profile.csip.set_coordinator.enabled=true \
+    bluetooth.profile.gatt.enabled=true \
+    bluetooth.profile.hap.client.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true \
+    bluetooth.profile.hid.device.enabled=true \
+    bluetooth.profile.hid.host.enabled=true \
+    bluetooth.profile.map.server.enabled=true \
+    bluetooth.profile.mcp.server.enabled=true \
+    bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pan.nap.enabled=true \
+    bluetooth.profile.pan.panu.enabled=true \
+    bluetooth.profile.pbap.server.enabled=true \
+    bluetooth.profile.sap.server.enabled=true \
+    bluetooth.profile.tbs.server.enabled=true \
+    bluetooth.profile.vc.server.enabled=true
 
 # Component overrides
 PRODUCT_COPY_FILES += \
