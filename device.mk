6feb362a2a0ffef4b7da47c96cb245c30d90a26d
Carter Hsu <carterhsu@google.com>
2022-02-08 17:33:35 +0800

    audio: Update Audio HIDL to 7.1
    Issue: FP3-A13#2
    Bug: 205235270
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ic54e2787ef27022556430c6c1db346b1e1348e39
    (cherry picked from commit 8a44f52082b1afbfe4643a03a95d8f6f48449fb3)
    (cherry picked from commit 26dd35f2bd9033cbe512b71ef542edc190c87443)

--- device.mk
+++ device.mk
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0 \
     android.hardware.audio.common@7.0 \
     android.hardware.audio.common@7.0-util \
-    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@7.0 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
