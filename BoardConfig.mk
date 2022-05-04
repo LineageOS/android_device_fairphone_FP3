LineageOS/android_device_fairphone_FP3

commit 4b6d6dda6e76518b9f0983cb5c3ef97b4394b98a

FP3: AB_OTA_UPDATER is BoardConfig variable
Bug: 130433003
Test: TreeHugger
Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

BoardConfig.mk
@@ -20,6 +20,14 @@ BOARD_VENDOR := Fairphone
 
 TARGET_ENFORCE_QSSI := true
 
+# A/B updater
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    vendor
+
 # APEX image
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 

device.mk
@@ -28,16 +28,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# A/B related defines
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    vbmeta \
-    vendor
-
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl.recovery \
     bootctrl.msm8953.recovery

commit 90a72328de2a9b05ec314bded1645da0f5fb4198

FP3: Add vbmeta to AB_OTA_PARTITIONS list
Needed to OTA update and sideload OTA/flashable packages.

Fixes: 4b6d6dd ("FP3: AB_OTA_UPDATER is BoardConfig variable")
Change-Id: Icf518a40059c752bd9a610ca7a25d7d1805e4224

BoardConfig.mk
@@ -26,6 +26,7 @@ AB_OTA_PARTITIONS += \
     boot \
     dtbo \
     system \
+    vbmeta \
     vendor
 
 # APEX image