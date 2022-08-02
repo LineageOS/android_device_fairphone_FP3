1ee70014a6cfbf54c7b2609430f4151899bc0920
Bharath <bharath@teamb58.org>
2022-08-02 12:25:52 +0530

    FP3: Update blobs from FP3-REL-8901.4.A.0013.4-gms-c1b5afb7-user
    Change-Id: Ida3146c1939e3e5736cda55b0ffdcaaf66d2764c

--- lineage_FP3.mk
+++ lineage_FP3.mk
@@ -39,7 +39,7 @@ PRODUCT_SYSTEM_DEVICE := FP3
 # userdebug
 PRODUCT_BUILD_PROP_OVERRIDES += \
         TARGET_DEVICE=FP3 \
-        PRIVATE_BUILD_DESC="FP3-user 11 8901.4.A.0010-gms-f74c58a8 release-keys"
+        PRIVATE_BUILD_DESC="FP3-user 11 8901.4.A.0013.4-gms-c1b5afb7 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Fairphone/FP3/FP3:11/8901.4.A.0010/gms-f74c58a8:user/release-keys
+BUILD_FINGERPRINT := Fairphone/FP3/FP3:11/8901.4.A.0013.4/gms-c1b5afb7:user/release-keys
