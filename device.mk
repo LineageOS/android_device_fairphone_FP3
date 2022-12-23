900b75fed6403e78fc36b7ee8b9277360da6685d
Bharath <bharath@teamb58.org>
2022-12-23 13:23:20 +0530

    FP3: media: Restore some of the essential media files
    * Restore following files that were removed accidentally in commit
    2fc0cdf70:
      * media_codecs_vendor.xml
      * media_codecs_vendor_audio.xml
      * media_profiles_8953_v1.xml
    
    Change-Id: I87c5dea91e07a6f08c850ef11f1c0395437f8ba1

--- device.mk
+++ device.mk
@@ -436,7 +436,10 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/media/media_profiles_8953_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_8953_v1.xml \
+    $(LOCAL_PATH)/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
