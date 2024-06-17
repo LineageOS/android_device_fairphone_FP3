a8a8bd151d432a881a8c0894698573e55464d855
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    FP3: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489
    (cherry picked from commit 62633708a10fce58cf789cbd8056bc760a63521a)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,7 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
