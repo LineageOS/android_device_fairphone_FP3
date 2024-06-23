584e848c99f39b42488d8f8290a0baac0426fb0d
Ankit Siddhapura <ankit.siddhapura@siliconsignals.io>
2024-06-23 22:31:10 +0530

    FP3: Inherit from QTI FCM
    Change-Id: I8946a4361bffa7e596b6641582c9601b0207a233

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,6 +182,8 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
