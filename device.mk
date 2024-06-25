477b5a953e2ca43c93e9a54d8ec68df96a4f29d1
Georg Veichtlbauer <georg@vware.at>
2024-06-25 23:02:34 +0200

    FP3: Build vendor variant of libpower
    Change-Id: Id7fe8a2598ffb93be4635424c86100a88aea8780

--- device.mk
+++ device.mk
@@ -666,6 +666,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
+    libpower.vendor \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
