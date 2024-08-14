0f4d82a1c706678b307f03c3be0d71e9c6c36d61
Aaron Kling <webgeek1234@gmail.com>
2024-08-14 01:44:45 -0500

    FP3: Enable elf checks
    Change-Id: Ia6c9a381abb3384c77532d75e02fd84c841a4d1e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -16,7 +16,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
-export TARGET_ENABLE_CHECKELF=false
+export TARGET_ENABLE_CHECKELF=true
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
