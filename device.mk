c8508000cdaed610329db173a8fa409d8f421f59
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:42:57 +0200

    FP3: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ib8ae3114ffae6ea5457cb9829fbbecfd7f065b66

--- device.mk
+++ device.mk
@@ -231,7 +231,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     gralloc.msm8953 \
-    hwcomposer.msm8953 \
+    hwcomposer.qcom \
     libdisplayconfig \
     libqdMetaData \
     libtinyxml \
