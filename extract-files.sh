ff20101a9b3582dd66f11334cf96fa13d6efc723
Bharath <bharath@teamb58.org>
2025-03-29 23:53:38 +0530

    Revert "FP3: extract-files: Add blob_fixup"
    This reverts commit 54baba3a2318064db4e3ac7b9556e1fdbd27cd1e.
    
    These xml files are already patched in stock.
    
    Change-Id: Iecf931a51311a357ea41928472c3d3d45c6d840f

--- extract-files.sh
+++ extract-files.sh
@@ -62,12 +62,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        # Fix xml version
-        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-            [ "$2" = "" ] && return 0
-            sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
-            ;;
-
         # Change soname for fingerprint.default.so.
         vendor/lib64/hw/fingerprint.FP3.so)
             [ "$2" = "" ] && return 0
