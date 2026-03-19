bb0ab331856b67584d3d21c2784950bbee564071
Bharath <bharath@teamb58.org>
2026-03-19 19:32:04 +0530

    FP3: Update blobs from FP3-6.A.040.2-gms-d33dc62f-user
    Change-Id: If5bba2c3d5bbae0d93aba15261bb471c9a9eab14

--- lineage_FP3.mk
+++ lineage_FP3.mk
@@ -40,5 +40,5 @@ PRODUCT_SYSTEM_DEVICE := FP3
 PRODUCT_BUILD_PROP_OVERRIDES += \
         DeviceName=FP3 \
         SystemName=FP3 \
-        BuildDesc="FP3-user 13 6.A.039.7-gms-44ba459b release-keys" \
-        BuildFingerprint = Fairphone/FP3/FP3:13/6.A.039.7/gms-44ba459b:user/release-keys
+        BuildDesc="FP3-user 13 6.A.040.2-gms-d33dc62f release-keys" \
+        BuildFingerprint = Fairphone/FP3/FP3:13/6.A.040.2/gms-d33dc62f:user/release-keys
